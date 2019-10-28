6fe1f2a878ef36cf9df9a2c6ce2c3e5b38dd3b8b
Jacob Stoppel <53243360+MorphiOs@users.noreply.github.com>
2019-10-28 02:26:04 -0500

    Add files via upload

---
+++ LithoGratos/Window1.xaml.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Owner
+ * Date: 10/25/2019
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+using ImageMagick;
+
+namespace LithoGratos
+{
+    public class LGsettings
+    {
+        public string L1default { get; set; }
+        public string L2default { get; set; }
+        public string L3default { get; set; }
+        public string L4default { get; set; }
+        public string LOGOdefault { get; set; }
+        public string fontPath { get; set; }
+    }
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        void button1_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fdPole = new OpenFileDialog();
+            fdPole.Filter = "JPEG files (*.jpg or *.jpeg)|*.jpg;*.jpeg";
+            if (fdPole.ShowDialog() == true) {
+                poleLoc.Text = fdPole.FileName;
+                pole.Source = new BitmapImage(new Uri(fdPole.FileName));
+            }
+        }
+        void settings_Click(object sender, RoutedEventArgs e)
+        {
+            WindowSet settingsd = new WindowSet();
+            settingsd.ShowDialog();
+        }
+        void about_Click(object sender, RoutedEventArgs e)
+        {
+            if (bdrAbout.Visibility == Visibility.Collapsed) {
+                bdrAbout.Visibility = Visibility.Visible;
+            } else {
+                bdrAbout.Visibility = Visibility.Collapsed;
+            }
+        }
+        void magic_Click(object sender, RoutedEventArgs e)
+        {
+            string strOutput = poleLoc.Text.Insert(poleLoc.Text.Length - 5, "(1)");
+            using (MagickImage image = new MagickImage(poleLoc.Text)){
+                new Drawables()
+                    .FontPointSize(100)
+                    .Font("Tahoma")
+                    .TextUnderColor(new MagickColor("#CCCCCCA0"))
+                    .FillColor(new MagickColor("black"))
+                    .Text(10,100, lineOne.Text + "\n" + lineTwo.Text + "\n" + lineThree.Text + "\n" + lineFour.Text)
+                    .Draw(image);
+                new Drawables().FillColor(new MagickColor("white"))
+                    .Rectangle(0,3381,750,3781);
+// Place logo code here followed by .Draw(image);
+                image.Write(strOutput);
+            }
+        }
+    }
+}
---
+++ LithoGratos/WindowSet.xaml.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Owner
+ * Date: 10/27/2019
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Xml.Serialization;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace LithoGratos
+{
+    /// <summary>
+    /// Interaction logic for WindowSet.xaml
+    /// </summary>
+    public partial class WindowSet : Window
+    {
+        public WindowSet()
+        {
+            InitializeComponent();
+            if(File.Exists("LithoGratosSettings.xml"))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<LGsettings>));
+                using (Stream reader = new FileStream("LithoGratosSettings.xml", FileMode.Open)) {
+                    List<LGsettings> setList = (List<LGsettings>)serializer.Deserialize(reader); 
+                }
+            }
+        }
+    }
+}
